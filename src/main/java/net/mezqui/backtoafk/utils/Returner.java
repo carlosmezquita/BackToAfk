6bc74d2bf8ef785b530f10fd95c1c9c9d18434e2
Carlos <mezquitalobato@gmail.com>
2022-01-25 11:39:03 +0100

    Switched /server survival to /server senior

--- src/main/java/net/mezqui/backtoafk/utils/Returner.java
+++ src/main/java/net/mezqui/backtoafk/utils/Returner.java
@@ -6,21 +6,29 @@ import net.minecraft.text.LiteralText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.registry.Registry;
 
+import static net.mezqui.backtoafk.BackToAfk.isActive;
 import static net.minecraft.client.MinecraftClient.getInstance;
 
 public class Returner {
     static MinecraftClient mc = getInstance();
     private static int ticks;
+    private static int attempt = 0;
 
     public static void tpBack(boolean isActive) {
-        String currentBiome = "";
-        if (mc.world != null && mc.player != null) {
-            currentBiome = String.valueOf(mc.world.getRegistryManager().get(Registry.BIOME_KEY).getId(mc.world.getBiome(mc.player.getBlockPos())));
-        }
+
+        String currentBiome = getBiome();
+
         if (isActive && currentBiome.equals("minecraft:the_void")){
                    mc.inGameHud.getChatHud().addMessage(new LiteralText("You are at the hub so you will be teleported in 30s.").formatted(Formatting.GREEN));
                    timerTeleport();
         }
+    }
+
+    private static String getBiome() {
+        if (mc.world != null && mc.player != null) {
+            return String.valueOf(mc.world.getRegistryManager().get(Registry.BIOME_KEY).getId(mc.world.getBiome(mc.player.getBlockPos())));
+        }
+        return null;
     }
 
 
@@ -28,10 +36,13 @@ public class Returner {
         ticks =20*30;
         ClientTickEvents.START_CLIENT_TICK.register(
                 (MinecraftClient) -> {
-                    if (ticks >= 0 && --ticks == 0 && mc.player != null) {
-                        mc.player.sendChatMessage("/server survival");
+                            if (ticks >= 0 && --ticks == 0 && mc.player != null) {
+                                mc.player.sendChatMessage("/server senior");
+                                ticks =20*30;
+
                     }
-                });
+                }
+                );
     }
 
 
