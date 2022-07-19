cb1d73c2c0b7dd193afc4e970368ddf29e31466d
Carlos <mezquitalobato@gmail.com>
2022-07-20 01:42:11 +0200

    Updated to MC 1.19

--- src/main/java/net/mezqui/backtoafk/BackToAfk.java
+++ src/main/java/net/mezqui/backtoafk/BackToAfk.java
@@ -1,16 +1,15 @@
 package net.mezqui.backtoafk;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.mezqui.backtoafk.utils.Returner;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.literal;
 
 public class BackToAfk implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -21,37 +20,29 @@ public class BackToAfk implements ModInitializer {
 
 	private int ticks = -1;
 	public static boolean isActive = false;
-
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
-		ClientCommandManager.DISPATCHER.register(
-				literal("bafk").then(literal("on").executes(context -> {
-					context.getSource().sendFeedback(new LiteralText("BackToAfk is now ON").formatted(Formatting.GREEN));
-					isActive = true;
-					return 0;
-				})
+		LOGGER.info("Initializing BeAfk!");
 
-		));
-		ClientCommandManager.DISPATCHER.register(
-				literal("bafk").then(literal("off").executes(context -> {
-							context.getSource().sendFeedback(new LiteralText("BackToAfk is now OFF.").formatted(Formatting.RED));
-							isActive = false;
-							return 0;
-						})
-
-				));
-		ClientCommandManager.DISPATCHER.register(
-				literal("bafk").executes(context -> {
-							context.getSource().sendFeedback(new LiteralText("BackToAfk is now set to: " +isActive).formatted(Formatting.GOLD));
-							return 0;
-						})
-
-				);
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("bafk").then(ClientCommandManager.literal("on").executes(context -> {
+			context.getSource().sendFeedback(Text.literal("BackToAfk is now ON").formatted(Formatting.GREEN));
+			isActive = true;
+			return 0;
+		}))));
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("bafk").then(ClientCommandManager.literal("off").executes(context -> {
+			context.getSource().sendFeedback(Text.literal("BackToAfk is now OFF.").formatted(Formatting.RED));
+			isActive = false;
+			return 0;
+		}))));
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("bafk").executes(context -> {
+			String status = isActive ? "on" : "off";
+			context.getSource().sendFeedback(Text.literal("BackToAfk is now " +status).formatted(Formatting.GOLD));
+			return 0;
+		})));
 
 		ClientPlayConnectionEvents.JOIN.register(
 				((handler, sender, client) -> {
--- src/main/java/net/mezqui/backtoafk/utils/Returner.java
+++ src/main/java/net/mezqui/backtoafk/utils/Returner.java
@@ -2,31 +2,29 @@ package net.mezqui.backtoafk.utils;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.registry.Registry;
 
-import static net.mezqui.backtoafk.BackToAfk.isActive;
+import java.util.Objects;
+
 import static net.minecraft.client.MinecraftClient.getInstance;
 
 public class Returner {
     static MinecraftClient mc = getInstance();
     private static int ticks;
-    private static int attempt = 0;
 
     public static void tpBack(boolean isActive) {
 
-        String currentBiome = getBiome();
-
-        if (isActive && currentBiome.equals("minecraft:the_void")){
-                   mc.inGameHud.getChatHud().addMessage(new LiteralText("You are at the hub so you will be teleported in 30s.").formatted(Formatting.GREEN));
+        if (isActive && Objects.equals(getBiome(), "minecraft:the_void")){
+                   mc.inGameHud.getChatHud().addMessage(Text.literal("You are at the hub so you will be teleported in 30s.").formatted(Formatting.GREEN));
                    timerTeleport();
         }
     }
 
     private static String getBiome() {
         if (mc.world != null && mc.player != null) {
-            return String.valueOf(mc.world.getRegistryManager().get(Registry.BIOME_KEY).getId(mc.world.getBiome(mc.player.getBlockPos())));
+            return String.valueOf(mc.world.getRegistryManager().get(Registry.BIOME_KEY).getId(mc.world.getBiome(mc.player.getBlockPos()).value()));
         }
         return null;
     }
