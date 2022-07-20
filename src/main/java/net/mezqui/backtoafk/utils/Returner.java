abb89a6f40e488b0ad97ec06436da6f66fb167d9
Carlos <mezquitalobato@gmail.com>
2022-07-20 03:18:50 +0200

    Updated to MC 1.19

--- src/main/java/net/mezqui/backtoafk/utils/Returner.java
+++ src/main/java/net/mezqui/backtoafk/utils/Returner.java
@@ -8,19 +8,19 @@ import net.minecraft.util.registry.Registry;
 
 import java.util.Objects;
 
+import static net.mezqui.backtoafk.BackToAfk.isActive;
 import static net.minecraft.client.MinecraftClient.getInstance;
 
 public class Returner {
     static MinecraftClient mc = getInstance();
-    private static int ticks;
+//    private static int ticks;
 
     public static void tpBack(boolean isActive) {
-
         if (isActive && Objects.equals(getBiome(), "minecraft:the_void")){
-                   mc.inGameHud.getChatHud().addMessage(Text.literal("You are at the hub so you will be teleported in 30s.").formatted(Formatting.GREEN));
-                   timerTeleport();
+                   timerTeleport(0);
         }
     }
+
 
     private static String getBiome() {
         if (mc.world != null && mc.player != null) {
@@ -30,17 +30,32 @@ public class Returner {
     }
 
 
-    private static void timerTeleport() {
-        ticks =20*30;
-        ClientTickEvents.START_CLIENT_TICK.register(
-                (MinecraftClient) -> {
-                            if (ticks >= 0 && --ticks == 0 && mc.player != null) {
-                                mc.player.sendChatMessage("/server senior");
-                                ticks =20*30;
+    private static void timerTeleport(int attempt) {
+        attempt++;
+        final int[] finalAttempt = {attempt};
+        final int[] ticks = {20 * 30 * finalAttempt[0]};
+        final int[] msgTicks = {20*5};
 
-                    }
-                }
+        ClientTickEvents.START_CLIENT_TICK.register(
+                        (MinecraftClient) -> {
+                            if (msgTicks[0] >= 0 && --msgTicks[0] == 0 && mc.player != null && Objects.equals(getBiome(), "minecraft:the_void")) {
+                             mc.inGameHud.getChatHud().addMessage(Text.literal("You are at the hub so you will be teleported in "+30* finalAttempt[0] +"s. ("+ finalAttempt[0] +"/5)").formatted(Formatting.GREEN));
+                            }
+                            if (ticks[0] >= 0 && --ticks[0] == 0 && mc.player != null && Objects.equals(getBiome(), "minecraft:the_void")) {
+//                                mc.player.sendChatMessage("/server senior");
+                                if (finalAttempt[0] <= 5 && isActive && Objects.equals(getBiome(), "minecraft:the_void")){
+                                    mc.player.sendCommand("server senior");
+                                    timerTeleport(finalAttempt[0]);
+                                }
+                            }
+                        }
                 );
+
+
+    }
+
+    private static void checkBiome(int attempt) {
+
     }
 
 
